59078196ea373cd041a215d0173facc98a4b640f
Dewmin <dewmin.vinuraka2@gmail.com>
2023-06-30 18:30:29 +0530

    create a application and build a database

---
+++ Cafe86DataBase/dbo.CafeTable.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[CafeTable] (
+    [Item_Code]        NCHAR (20)    NOT NULL,
+    [Item_Price]       INT           NOT NULL,
+    [Item_Qty]         INT           NOT NULL,
+    [Total_Price]      INT           NOT NULL,
+    [Customer_Name]    NCHAR (30)    NOT NULL,
+    [Customer_Code]    INT           NOT NULL,
+    [Customer_TP]      INT           NOT NULL,
+    [Customer_Address] NVARCHAR (50) NOT NULL,
+    PRIMARY KEY CLUSTERED ([Customer_Code] ASC)
+);
+
